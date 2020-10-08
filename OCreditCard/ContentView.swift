5f85c4411f112216f39e2bd5fd06f149143d4641
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-09 00:45:34 +0300

    Update ContentView.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- OCreditCard/ContentView.swift
+++ OCreditCard/ContentView.swift
@@ -8,11 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: - PROPERTIES
+    
+    @State private var degrees: Double = 0
+    @State private var flipped: Bool = false
+    
+    // MARK: - BODY
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        CreditCard {
+            
+            VStack {
+                Group {
+                    if flipped {
+                        CreditCardBack()
+                    } else {
+                        CreditCardFront()
+                    }
+                }
+            }
+            .rotation3DEffect(
+                .degrees(degrees),
+                axis: (x: 0.0, y: 1.0, z: 0.0),
+                anchor: .center,
+                anchorZ: 0.0,
+                perspective: 1.0
+            )
+        }
+        .onTapGesture(count: 1, perform: {
+            
+            withAnimation {
+                
+                degrees += 180
+                flipped.toggle()
+            }
+        })
     }
 }
+
+// MARK: - PREVIEW
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
