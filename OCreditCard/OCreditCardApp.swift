30ee9d282c82c0d6a1028e186dcb4b5de4c479c6
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-09 00:28:13 +0300

    Initial Commit

---
+++ OCreditCard/OCreditCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  OCreditCardApp.swift
+//  OCreditCard
+//
+//  Created by Abdelrahman Mohamed on 9.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct OCreditCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
