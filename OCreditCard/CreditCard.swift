b55dab61c490f5c5d2f14b70aa4432c48f0c9f9f
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-09 00:30:31 +0300

    Create CreditCard.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ OCreditCard/CreditCard.swift
@@ -0,0 +1,27 @@
+//
+//  CreditCard.swift
+//  OCreditCard
+//
+//  Created by Abdelrahman Mohamed on 9.10.2020.
+//
+
+import SwiftUI
+
+struct CreditCard: View {
+    
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct CreditCard_Previews: PreviewProvider {
+    static var previews: some View {
+        CreditCard()
+    }
+}
